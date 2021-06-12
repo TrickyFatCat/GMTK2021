8d37602fdebc53f3752eae6ee57925302a846ca2
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-06-12 11:11:41 +0300

    Create battery station class

---
+++ Source/GMTK_2021/Private/BatteryStation.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BatteryStation.h"
+#include "PlayerCharacter.h"
+#include "Components/SphereComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+
+ABatteryStation::ABatteryStation()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	TriggerSphere = CreateDefaultSubobject<USphereComponent>("TriggerSphere");
+	SetRootComponent(TriggerSphere);
+
+	SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>("SkeletalMesh");
+	SkeletalMesh->SetupAttachment(GetRootComponent());
+}
+
+void ABatteryStation::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABatteryStation::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+bool ABatteryStation::ProcessInteraction_Implementation(APlayerCharacter* PlayerCharacter)
+{
+	return true;
+}
+
---
+++ Source/GMTK_2021/Public/BatteryStation.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Interact.h"
+#include "GameFramework/Actor.h"
+#include "BatteryStation.generated.h"
+
+class USkeletalMeshComponent;
+class USphereComponent;
+
+UCLASS()
+class GMTK_2021_API ABatteryStation : public AActor, public IInteract
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABatteryStation();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	bool ProcessInteraction_Implementation(APlayerCharacter* PlayerCharacter);
+
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Components", meta=(AllowPrivateAccess="true"))
+	USphereComponent* TriggerSphere = nullptr;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Components", meta=(AllowPrivateAccess="true"))
+	USkeletalMeshComponent* SkeletalMesh = nullptr;
+};
