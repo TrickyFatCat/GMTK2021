b009e035c2d2689686d9b480eb224e8869040190
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-06-12 11:11:24 +0300

    Create interaction interface

---
+++ Source/GMTK_2021/Public/Interact.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Interact.generated.h"
+
+class APlayerCharacter;
+
+UINTERFACE(BlueprintType)
+class UInteract : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class GMTK_2021_API IInteract
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	bool ProcessInteraction(APlayerCharacter* PlayerCharacter);
+};
